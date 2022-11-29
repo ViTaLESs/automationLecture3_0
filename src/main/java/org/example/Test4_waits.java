4f63e23a42a3bfa051b8147ea14fcaac451df1f9
Vitalii Laputko <vitalbka.91@gmail.com>
2022-11-29 12:45:01 +0200

    updates Test4_waits.java

--- src/main/java/org/example/Test4_waits.java
+++ src/main/java/org/example/Test4_waits.java
@@ -16,58 +16,61 @@ public class Test4_waits {
     static Logger log = Logger.getLogger(Test4_waits.class);
     static WebDriver driver;
 
+    static By playerSearchField = By.xpath(".//div[@class='ant-card-body']//input[@data-qa-modifier='player-search']");
+    static By loginInputLocator = By.xpath(".//input[@type='email']");
+    static By passInputLocator = By.xpath(".//input[@name='passwd']");
+    static By submitButton = By.xpath(".//input[@type='submit']");
+    static By userBalance = By.cssSelector("tr[data-row-key='balance'] span");
+
     static String siteUrl = "url";
     static String login = "login";
     static String pass = "pass";
-
-    //TODO:
-    // 1. Show the locators
-    // 2. Sho9w Ignore file
-    //By locator = By.xpath(".//div[@class='ant-card-body']//input[@data-qa-modifier='player-search']");
-
+    static String uid = "175598218";
 
     public static void main(String[] args) {
         driver = new ChromeDriver();
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+        goToUrl(siteUrl);
         loginToTheSystem();
         maximizeBrowserWindow();
 
         new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//div[@class='ant-card-body']//input[@data-qa-modifier='player-search']")));
-        WebElement searchInputField = driver.findElement(By.xpath(".//div[@class='ant-card-body']//input[@data-qa-modifier='player-search']"));
-        searchInputField.sendKeys("175598218");
-        log.info("Type test user id into player search field - 175598218");
+                .until(ExpectedConditions.visibilityOfElementLocated(playerSearchField));
+
+        WebElement searchInputField = driver.findElement(playerSearchField);
+        searchInputField.sendKeys(uid);
+        log.info("Type test user id into player search field - " + uid);
         searchInputField.sendKeys(Keys.ENTER);
 
-        driver.findElement(By.cssSelector("tr[data-row-key='balance'] span"));
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(userBalance));
 
         //driver.quit();
     }
 
     private static void loginToTheSystem(){
-        driver.get(siteUrl);
-        log.debug("Switch to url - " + siteUrl);
-
         //Fill login field
-        By loginInputLocator = By.xpath(".//input[@type='email']");
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(loginInputLocator));
 
         driver.findElement(loginInputLocator).sendKeys(login);
         log.info("Send auto test user email");
-
         driver.findElement(loginInputLocator).sendKeys(Keys.ENTER);
 
         //Fill password field
         new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//input[@name='passwd']")));
-        WebElement passInput = driver.findElement(By.xpath(".//input[@name='passwd']"));
+                .until(ExpectedConditions.visibilityOfElementLocated(passInputLocator));
+
+        WebElement passInput = driver.findElement(passInputLocator);
         passInput.sendKeys(pass);
         log.info("Send auto test user pass");
 
-        WebElement buttonNext = driver.findElement(By.xpath(".//input[@type='submit']"));
-        buttonNext.click();
+        driver.findElement(submitButton).click();
+    }
+
+    public static void goToUrl(String siteUrl){
+        driver.get(siteUrl);
+        log.debug("Switch to url - " + siteUrl);
     }
 
     private static void maximizeBrowserWindow(){
